2baa0b3056aa8cd82c37fbaf6e2b78064a7bd05a
stopsilver <hilljeong@gmail.com>
2017-01-04 20:36:48 +0900

    syc 20170104, 통신 성공, should develop decoding after receive data

--- src/main/EchoClientInboundHandler.java
+++ src/main/EchoClientInboundHandler.java
@@ -1,9 +1,13 @@
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.CompositeByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Jieun on 2017. 1. 2..
@@ -12,17 +16,64 @@ public class EchoClientInboundHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
-        String sendMessage = "Hey Server get this!";
 
-        ByteBuf messageBuffer = Unpooled.buffer();
-        messageBuffer.writeBytes(sendMessage.getBytes());
+        ByteBuf CommunicationHeader = Unpooled.buffer(60);
+        ByteBuf DataHeader = Unpooled.buffer(100);
+        ByteBuf DataBody = Unpooled.buffer(300);
+        ByteBuf Filler = Unpooled.buffer(100);
+
+        DataHeader.setZero(0, DataHeader.writableBytes());
+        DataBody.setZero(0, DataBody.writableBytes());
+        Filler.setZero(0, Filler.writableBytes());
+
+
+        byte[] Stx = {0x02};
+        String Version = "v1.0";
+        String MsgLen = "000160";
+        String ApType = "C115TR";
+
+        SimpleDateFormat formatter = new SimpleDateFormat ( "yyyyMMddHHmmssSSSSSS", Locale.KOREA );
+        String SendingTime = formatter.format(new Date());
+
+        String Trcode = "LINK";
+
+        String SeqNo = "00000000000";
+        String DataCnt = "000";
+
+        CommunicationHeader.writeBytes(Stx);
+        CommunicationHeader.writeBytes(Version.getBytes());
+        CommunicationHeader.writeBytes(MsgLen.getBytes());
+        CommunicationHeader.writeBytes(ApType.getBytes());
+        CommunicationHeader.writeBytes(SendingTime.getBytes());
+        CommunicationHeader.writeBytes(Trcode.getBytes());
+        CommunicationHeader.writeBytes(SeqNo.getBytes());
+        CommunicationHeader.writeBytes(DataCnt.getBytes());
+
+        CommunicationHeader.setZero(CommunicationHeader.readableBytes(), CommunicationHeader.capacity() - CommunicationHeader.readableBytes());
+        System.out.println(CommunicationHeader.toString(Charset.defaultCharset()));
+
+//        sendMessage.addComponents(CommunicationHeader, DataHeader, DataBody, Filler);
+//
+//        for (ByteBuf buf: sendMessage) {
+//            System.out.println(buf.toString(Charset.defaultCharset()));
+//        }
+
+//        String Message = "Hello";
+//        ByteBuf sendMessage = Unpooled.buffer();
+//        sendMessage.writeBytes(Message.getBytes());
 
         StringBuilder builder = new StringBuilder();
         builder.append("Client sent: [");
-        builder.append(sendMessage);
+        String StxString = Integer.toHexString(Stx[0]);
+//        builder.append(Message);
+        builder.append(StxString + Version + MsgLen + ApType + SendingTime + Trcode + SeqNo + DataCnt);
         builder.append("]");
         System.out.println(builder.toString());
-        ctx.writeAndFlush(messageBuffer);
+        ctx.write(CommunicationHeader);
+        ctx.write(DataHeader);
+        ctx.write(DataBody);
+        ctx.writeAndFlush(Filler);
+//        ctx.writeAndFlush(sendMessage);
     }
 
     // 채널을 읽을 때 동작할 코드를 정의 합니다.
--- src/main/EchoServerInboundHandler.java
+++ src/main/EchoServerInboundHandler.java
@@ -1,9 +1,13 @@
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import static java.lang.String.valueOf;
 
 public class EchoServerInboundHandler extends ChannelInboundHandlerAdapter {
 
@@ -12,18 +16,18 @@ public class EchoServerInboundHandler extends ChannelInboundHandlerAdapter {
     }
 
     public void channelActive(ChannelHandlerContext ctx) {
-        String sendMessage = "You are connected to Server";
-
-        ByteBuf messageBuffer = Unpooled.buffer();
-
-        messageBuffer.writeBytes(sendMessage.getBytes());
-
-//        StringBuilder builder = new StringBuilder();
-//        builder.append("전송한 문자열 [");
-//        builder.append(sendMessage);
-//        builder.append("]");
-//        System.out.println(builder.toString());
-        ctx.writeAndFlush(messageBuffer);
+//        String sendMessage = "You are connected to Server";
+//
+//        ByteBuf messageBuffer = Unpooled.buffer();
+//
+//        messageBuffer.writeBytes(sendMessage.getBytes());
+//
+////        StringBuilder builder = new StringBuilder();
+////        builder.append("전송한 문자열 [");
+////        builder.append(sendMessage);
+////        builder.append("]");
+////        System.out.println(builder.toString());
+//        ctx.writeAndFlush(messageBuffer);
     }
 
     public void channelInactive(ChannelHandlerContext ctx) {
@@ -32,13 +36,118 @@ public class EchoServerInboundHandler extends ChannelInboundHandlerAdapter {
     // 채널을 읽을 때 동작할 코드를 정의 합니다.
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         System.out.println("in channelRead");
-//		ByteBuf input = (ByteBuf) msg;
-//		byte[] bytes = input.array();
-//		System.out.println(bytes);
-        String readMessage = ((ByteBuf) msg).toString(Charset.defaultCharset());
 
-        System.out.println("Server got: [" + readMessage + ']');
-        ctx.write(msg); // 메시지를 그대로 다시 write 합니다.
+        ByteBuf input = (ByteBuf) msg;
+
+        ByteBuf CommunicationHeader = input.copy(0, 60);
+        ByteBuf DataHeader = input.copy(60, 160);
+        ByteBuf DataBody = input.copy(160, 300);
+        ByteBuf Filler = input.copy(460, 100);
+
+        HashMap<String, ByteBuf> InputValue = new HashMap<String, ByteBuf>();
+
+        String[] CHComposition = {"Stx", "Version", "Len", "ApType", "SendingTime", "TRcode", "SeqNo", "DataCnt", "HeaderFiller"};
+        Integer[] CHlength = {1, 4, 7, 6, 20, 4, 11, 3, 4};
+        Object[] Type = {};
+
+        int num = 0;
+        for (int i=0; i<CHComposition.length; i++) {
+            ByteBuf slice = CommunicationHeader.slice(num, CHlength[i]);
+            InputValue.put(CHComposition[i], slice);
+            num += CHlength[i];
+        }
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Server got: [");
+
+        for (String i: CHComposition) {
+            ByteBuf buf = InputValue.get(i);
+
+            if (i.equals("Stx") || i.equals("HeaderFiller")) {
+                for (int j = 0; j < buf.readableBytes(); j++) {
+                    int k = (int) buf.getByte(j);
+                    String str = valueOf(k);
+                    System.out.println(str);
+                    builder.append(str);
+                }
+            }
+            else {
+                builder.append(InputValue.get(i).toString(Charset.defaultCharset()));
+            }
+        }
+
+        builder.append("]");
+        System.out.println(builder.toString());
+
+
+        ByteBuf TRcode = InputValue.get("TRcode");
+        ByteBuf SendingTimeBuf = InputValue.get("SendingTime");
+
+        System.out.println("60");
+
+        if (TRcode.hasArray()) {
+            if (Arrays.equals(TRcode.array(), "LINK".getBytes())) {
+                TRcode.clear();
+                TRcode.writeBytes("LIOK".getBytes());
+                InputValue.put("Stx", TRcode);
+            }
+        }
+
+        System.out.println("80");
+        SimpleDateFormat formatter = new SimpleDateFormat ( "yyyyMMddHHmmssSSSSSS", Locale.KOREA );
+        String SendingTime = formatter.format(new Date());
+        SendingTimeBuf.clear();
+        SendingTimeBuf.writeBytes(SendingTime.getBytes());
+        InputValue.put("SendingTime", SendingTimeBuf);
+
+        CommunicationHeader.clear();
+
+        System.out.println("90");
+        for (String i: CHComposition) {
+            ByteBuf Bytebuf = InputValue.get(i);
+            CommunicationHeader.writeBytes(Bytebuf);
+        }
+
+        System.out.println("100");
+
+//        for (ByteBuf i: InputValue.values()) {
+//            String j;
+//            int a = i.getInt(0);
+//
+//            if (a == 0x02) {
+//                j = Integer.toHexString(a);
+//            }
+//            else {
+//                j = i.toString(Charset.defaultCharset());
+//            }
+//            System.out.println(j);
+//        }
+        StringBuilder builder1 = new StringBuilder();
+        builder1.append("Server sent: [");
+
+        for (String i: CHComposition) {
+            ByteBuf buf1 = InputValue.get(i);
+
+            if (i.equals("Stx") || i.equals("HeaderFiller")) {
+                for (int j = 0; j < buf1.readableBytes(); j++) {
+                    int k = (int) buf1.getByte(j);
+                    String str = valueOf(k);
+                    System.out.println(str);
+                    builder1.append(str);
+                }
+            }
+            else {
+                builder1.append(InputValue.get(i).toString(Charset.defaultCharset()));
+            }
+        }
+
+        builder1.append("]");
+        System.out.println(builder1.toString());
+
+        ctx.write(CommunicationHeader);
+        ctx.write(DataHeader);
+        ctx.write(DataBody);
+        ctx.writeAndFlush(Filler);
     }
 
     // 채널 읽는 것을 완료했을 때 동작할 코드를 정의 합니다.
@@ -46,14 +155,6 @@ public class EchoServerInboundHandler extends ChannelInboundHandlerAdapter {
         System.out.println("channelReadComplete");
         ctx.flush(); // 컨텍스트의 내용을 플러쉬합니다.
     }
-
-//	@Override
-//	public ChannelPipeline getPipeline() throws Exception {
-//		ChannelPipeline pipeline=Channels.pipeline();
-//		pipeline.addLast("lengthbasedframedecoder",new LengthFieldBasedFrameDecoder(MAX_FRAME_LENGTH,0,4,0,4));
-//		pipeline.addLast("mainhandler",this);
-//		return pipeline;
-//	}
 
     // 예외가 발생할 때 동작할 코드를 정의 합니다.
     @Override
